89f9ddf3a43d10bce2227d6e0b88884431f25b00
shri arthi <shrarthi7896@gmail.com>
2023-09-17 13:17:11 +0530

    launchbrowser

--- src/main/java/week1/day1/LaunchBrowser.java
+++ src/main/java/week1/day1/LaunchBrowser.java
@@ -1,8 +1,10 @@
 package week1.day1;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LaunchBrowser {
 
@@ -10,12 +12,22 @@ public class LaunchBrowser {
 		
 		//ChromeDriver driver =new ChromeDriver();
 		EdgeDriver driver =new EdgeDriver();
-		driver.get("https://www.facebook.com/");
+		driver.get("http://leaftaps.com/opentaps/");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("email")).sendKeys("6380208845");
-		driver.findElement(By.id("pass")).sendKeys("narayana");
-		driver.findElement(By.name("login")).click();
-		driver.findElement(By.linkText(null))
+		driver.findElement(By.name("USERNAME")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("center-content-column"));
+		driver.findElement(By.className("allSubSectionBlocks"));
+		WebElement element = driver.findElement(By.id("createLeadForm_dataSourceId"));
+		
+		Select object=new Select(element);
+		//object.selectByValue("LEAD_EMPLOYEE");
+		//object.selectByIndex(1);
+		object.selectByVisibleText("Existing Customer");
 		
 	
 	}
